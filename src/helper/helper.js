ba383e9153af82003d5b384483b960bf638f4c93
ho3einHamidi <hosseinmhh@yahoo.com>
2024-06-19 11:55:29 +0330

    feat: searchparams

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,4 +1,33 @@
 const shortenText = (text) => {
   return text.split(" ").slice(0, 3).join(" ");
 };
-export { shortenText };
+
+const searchProduct = (products, search) => {
+  if (!search) return products;
+  const searchedProduct = products.filter((product) => {
+    return product.title.toLowerCase().includes(search);
+  });
+
+  return searchedProduct;
+};
+
+const filterProduct = (products, category) => {
+  if (!category) return products;
+  const filteredProduct = products.filter(
+    (product) => product.category === category
+  );
+  return filteredProduct;
+};
+
+const createQueryObject = (currentQuery, newQuerry) => {
+  if (newQuerry.category === "all") {
+    const { category, ...rest } = currentQuery;
+    return rest;
+  }
+  if (newQuerry.search === "") {
+    const { search, ...rest } = currentQuery;
+    return rest;
+  }
+  return { ...currentQuery, ...newQuerry };
+};
+export { shortenText, searchProduct, filterProduct, createQueryObject };
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,21 +1,78 @@
-import { TailSpin } from "react-loader-spinner";
-import Card from "../Components/Card";
+import { useEffect, useState } from "react";
 import { useProduct } from "../context/ProductsContext";
+import { useSearchParams } from "react-router-dom";
+
+import Card from "../Components/Card";
 import styles from "./ProductsPage.module.css";
 import Loader from "../Components/Loader";
+import { ImSearch } from "react-icons/im";
+import { FaListUl } from "react-icons/fa";
+import {
+  createQueryObject,
+  filterProduct,
+  searchProduct,
+} from "../helper/helper";
 function ProductsPage() {
   const products = useProduct();
 
+  const [displayed, setDisplayed] = useState([]);
+
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState({});
+  const [searchParams, setSearchParams] = useSearchParams();
+  useEffect(() => {
+    setDisplayed(products);
+  }, [products]);
+
+  useEffect(() => {
+    setSearchParams(query);
+    let finalProducts = searchProduct(products, query.search);
+    finalProducts = filterProduct(finalProducts, query.category);
+    setDisplayed(finalProducts);
+  }, [query]);
+  const searchHandler = () => {
+    setQuery((query) => createQueryObject(query, { search }));
+  };
+  const categoryHandler = (e) => {
+    const { tagName } = e.target;
+    const category = e.target.innerText.toLowerCase();
+    setQuery((query) => createQueryObject(query, { category }));
+  };
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map((product) => (
-          <Card key={product.id} data={product} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLocaleLowerCase().trim())}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
       </div>
-      <div>sidebar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!displayed.length && <Loader />}
+          {displayed.map((product) => (
+            <Card key={product.id} data={product} />
+          ))}
+        </div>
+        <div>
+          <div>
+            <FaListUl />
+            <p>Categories</p>
+          </div>
+          <ul onClick={categoryHandler}>
+            <li>All</li>
+            <li>Electronics</li>
+            <li>Jewelery</li>
+            <li>Men's Clothing</li>
+            <li>Women's Clothing</li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
 
